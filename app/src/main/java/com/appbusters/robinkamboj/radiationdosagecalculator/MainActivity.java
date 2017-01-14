c14b8223d29111b9a3920c027ed6f5b2d93ae6c9
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-14 17:00:47 +0530

    implemented slider

--- app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/MainActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/MainActivity.java
@@ -1,15 +1,29 @@
 package com.appbusters.robinkamboj.radiationdosagecalculator;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,4 +69,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
