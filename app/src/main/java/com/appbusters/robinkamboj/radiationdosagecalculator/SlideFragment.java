f2d1b07dc2367f417e12e2a2acfd3e1e56e1890d
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-15 14:20:11 +0530

    gliding

--- app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/SlideFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/SlideFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 
 /**
@@ -13,6 +16,7 @@ import android.view.ViewGroup;
  */
 public class SlideFragment extends Fragment {
 
+    ImageView imageView1;
 
     public SlideFragment() {
         // Required empty public constructor
@@ -22,8 +26,19 @@ public class SlideFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slide, container, false);
+        View v = inflater.inflate(R.layout.fragment_slide, container, false);
+
+        imageView1 = (ImageView) v.findViewById(R.id.imageView1);
+        Glide.with(this)
+                .load("https://cdn.gamerant.com/wp-content/uploads/Batman-Arkham-City-Robin-Combat-Gadgets.jpg")
+                .placeholder(R.drawable.number)
+                .crossFade()
+                .centerCrop()
+                .into(imageView1);
+
+        return v;
     }
 
 }
