12974f1343de51377e298031e3303c68c9ce309b
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-25 22:07:09 +0530

    onClick

--- app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/view/slides/SlideFragment.java
+++ app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/view/slides/SlideFragment.java
@@ -1,14 +1,17 @@
 package com.appbusters.robinkamboj.radiationdosagecalculator.view.slides;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.appbusters.robinkamboj.radiationdosagecalculator.R;
+import com.appbusters.robinkamboj.radiationdosagecalculator.view.CalculateActivity;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
@@ -19,6 +22,7 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 public class SlideFragment extends Fragment {
 
     ImageView imageView1;
+    Button button;
 
     public SlideFragment() {
         // Required empty public constructor
@@ -32,13 +36,23 @@ public class SlideFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_slide, container, false);
 
+        button = (Button) v.findViewById(R.id.calculate);
         imageView1 = (ImageView) v.findViewById(R.id.imageView1);
+
         Glide.with(this)
                 .load("http://wallpapercave.com/wp/e1jWmMP.png")
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .crossFade()
                 .centerCrop()
                 .into(imageView1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), CalculateActivity.class);
+                startActivity(i);
+            }
+        });
 
         return v;
     }
