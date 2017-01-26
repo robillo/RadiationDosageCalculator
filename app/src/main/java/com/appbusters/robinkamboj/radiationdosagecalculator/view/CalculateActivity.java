3c6aa4ab6a784de7ade9b4cbd4d48b748606898c
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-26 16:51:17 +0530

    ...

--- app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/view/CalculateActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/view/CalculateActivity.java
@@ -21,14 +21,6 @@ public class CalculateActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         setupActionBar();
     }
 
@@ -40,7 +32,6 @@ public class CalculateActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
