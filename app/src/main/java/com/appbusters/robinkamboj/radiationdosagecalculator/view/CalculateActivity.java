acf2568f55e3065cf1b952c5a4046d3ed09c6482
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-26 13:50:41 +0530

    inflated menus to activities

--- app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/view/CalculateActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/radiationdosagecalculator/view/CalculateActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.appbusters.robinkamboj.radiationdosagecalculator.R;
@@ -26,6 +28,34 @@ public class CalculateActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //nonspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_more) {
+            return true;
+        }
+        if (id == R.id.action_logout) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
